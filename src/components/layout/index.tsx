f208bcaf5f6d788021ded1245c99a888cf0c37ef
Niv Zelber <nivznivz0@gamil.com>
2021-01-27 15:59:51 +0200

    move some files around and remove redundent files

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -10,8 +10,6 @@ import "./layout.css";
 import { graphql, useStaticQuery } from "gatsby";
 import React, { PropsWithChildren } from "react";
 
-import Header from "./header";
-
 interface Props {}
 
 const Layout = ({ children }: PropsWithChildren<Props>) => {
@@ -26,27 +24,24 @@ const Layout = ({ children }: PropsWithChildren<Props>) => {
   `);
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `0 1.0875rem 1.45rem`
+      }}
+    >
+      <main>{children}</main>
+      <footer
         style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`
+          marginTop: `2rem`
         }}
       >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.com">Gatsby</a>
+      </footer>
+    </div>
   );
 };
 
