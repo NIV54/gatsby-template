f53d4e138c92a40ecb7b9e7aded6d68e468b3f04
Niv Zelber <nivznivz0@gamil.com>
2021-01-27 16:16:24 +0200

    remove comments

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,10 +1,3 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import React, { PropsWithChildren } from "react";
 
 import { LayoutGlobalStyle } from "./layout.styled";
